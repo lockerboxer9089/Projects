2cac2d775eb2a13ea4bb51b33f228ca93f577ce3
lockerboxer9089 <79079765+lockerboxer9089@users.noreply.github.com>
2025-10-21 21:49:44 -0400

    Update main.py

--- Game_Launcher/main.py
+++ Game_Launcher/main.py
@@ -2,11 +2,50 @@ import pygame
 
 pygame.init()
 
+class Button:
+    def __init__(self, x, y, width, height, text, function=None, onePress=False):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.text = text
+        self.function = function
+        self.onePress = onePress
+        self.alreadyPressed = False
+        self.fillColors = {
+            'normal': '#ffffff',
+            'hover': '#666666',
+            'pressed': '#333333'
+        }
+        self.buttonSurface = pygame.Surface((self.width, self.height))
+        self.buttonRect = pygame.Rect(self.x, self.y, self.width, self.height)
+        self.buttonSurf = font.render(text, True, (20,20,20))
+        objects.append(self)
+
+    def process(self):
+        mouse_pos = pygame.mouse.get_pos()
+        self.buttonSurface.fill(self.fillColors['normal'])
+        if self.buttonRect.collidepoint(mouse_pos):
+            self.buttonSurface.fill(self.fillColors['hover'])
+            if pygame.mouse.get_pressed(num_buttons=3)[0]:
+                self.buttonSurface.fill(self.fillColors['pressed'])
+                if self.onePress:
+                    self.function()
+                elif not self.alreadyPressed:
+                    self.function()
+                    self.alreadyPressed = True
+            else:
+                self.alreadyPressed = True
+        
+        self.buttonSurface.blit(self.buttonSurf, [])
+
 WIDTH,HEIGHT = 600,600
+objects = []
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Game Launcher")
 clock = pygame.time.Clock()
+font = pygame.font.SysFont('Arial', 40)
 
 # Images
 hangman_logo = pygame.image.load("hangman_logo.jpg").convert()
