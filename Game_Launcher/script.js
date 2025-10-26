1b38378d530893b4e2360dfa1f7b3a4e4c0cf978
lockerboxer9089 <79079765+lockerboxer9089@users.noreply.github.com>
2025-10-25 20:26:24 -0400

    updated files
    created new folders for the separate html pages, added to the css by customizing some more buttons whose designs i got from other sources

--- Game_Launcher/script.js
+++ Game_Launcher/script.js
@@ -1,3 +1,15 @@
-function redirectHome() {
-    location.replace("https://google.com");
+function redirect(page) {
+    if (page == 1) {
+        location.replace("redirect_pages/home.html");
+    }
+    else if (page == 2) {
+        location.replace("redirect_pages/login.html");
+    }
+    else if (page == 3) {
+        location.replace("game_pages/hangman.html");
+    }
+}
+
+function returnToMain() {
+    location.replace("../index.html");
 }
