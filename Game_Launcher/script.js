6562575da022a7db6b368ad5133b399dcca619b7
lockerboxer9089 <79079765+lockerboxer9089@users.noreply.github.com>
2025-10-26 18:01:57 -0400

    update files

--- Game_Launcher/script.js
+++ Game_Launcher/script.js
@@ -20,3 +20,31 @@ function returnToMain() {
 function review() {
     location.replace("redirect_pages/review.html");
 }
+
+function submit() {
+    var fname = document.getElementById("fname").value;
+    var lname = document.getElementById("lname").value;
+    var email = document.getElementById("email").value;
+    var rating = document.querySelector('input[name="rating"]:checked')?.value || 'Not Provided';
+
+    var improvements = [];
+    if (document.getElementById("option1").checked) {
+        improvements.push(document.getElementById("option1").value);
+    }
+    if (document.getElementById("option2").checked) {
+        improvements.push(document.getElementById("option2").value);
+    }
+    if (document.getElementById("option3").checked) {
+        improvements.push(document.getElementById("option3").value);
+    }
+
+    sessionStorage.setItem('reviewData', JSON.stringify({
+        firstName: fname,
+        lastName: lname,
+        email: email,
+        rating: rating,
+        improvements: improvements
+    }));
+
+    location.replace("final.html");
+}
